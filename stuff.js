de96deabf937a907d359e207b4bbff6658a7023e
rainlin05 <rainl5@nycstudents.net>
2023-10-04 15:24:15 -0400

    hmm

---
+++ stuff.js
@@ -0,0 +1,9 @@
+function initialize() {
+    const num = Math.floor(Math.random()*99 + 1);
+    display();
+}
+function display() {
+    document.createElement("p");
+    document.createElement("p");
+
+}
