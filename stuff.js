bf4dfba9509714f799358d30dd30042ac0928d1f
rainlin05 <rainl5@nycstudents.net>
2023-10-10 23:41:25 -0400

    brain hurt

--- stuff.js
+++
@@ -1,8 +0,0 @@
-function initialize() {
-    const num = Math.floor(Math.random()*99 + 1);
-    display();
-}
-function display() {
-    document.createElement("p");
-    document.createElement("p");
-}
